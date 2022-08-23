afd640b7783390a0adef06ba505699d85b418a8a
roniecv <111037734+roniecv@users.noreply.github.com>
2022-08-23 23:02:57 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,10 +18,10 @@ locals {
   }
 
   #network_contracts  = flatten([for contract_name, contract_properties in local.contracts : contract_properties.network if can(contract_properties.network)])
-  #storage_contracts  = flatten([for contract_name, contract_properties in local.contracts : contract_properties.storage if can(contract_properties.storage)])
+  storage_contracts  = flatten([for contract_name, contract_properties in local.contracts : contract_properties.storage if can(contract_properties.storage)])
   #compute_contracts  = flatten([for contract_name, contract_properties in local.contracts : contract_properties.compute if can(contract_properties.compute)])
   #database_contracts = flatten([for contract_name, contract_properties in local.contracts : contract_properties.database if can(contract_properties.database)])
-   account_contracts  = flatten([for contract_name, contract_properties in local.contracts : contract_properties.account if can(contract_properties.account)])  
+  #account_contracts  = flatten([for contract_name, contract_properties in local.contracts : contract_properties.account if can(contract_properties.account)])  
 }
 
 #module "network" {
@@ -37,10 +37,10 @@ locals {
 #   private_subnets_ipv6_cidr_blocks = module.network.private_subnets_ipv6_cidr_blocks
 # }
 
-#module "storage" {
-#  source            = "./templates/storage"
-#  storage_contracts = local.storage_contracts
-#}
+module "storage" {
+  source            = "./templates/storage"
+  storage_contracts = local.storage_contracts
+}
 
 # module "compute" {
 #   source = "./templates/compute"
@@ -59,7 +59,7 @@ locals {
 #   database_contracts = local.database_contracts
 # }
 
-  module "ssoassignment" {
-  source            = "./templates/accounts"
-  account_contracts = local.account_contracts
-}
+ # module "ssoassignment" {
+ # source            = "./templates/accounts"
+ # account_contracts = local.account_contracts
+#}
